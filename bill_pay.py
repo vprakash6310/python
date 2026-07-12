14c1b367f46dbc3b578e88e00dd60ef2957afb70
vprakash6310 <vprakash6310@gmail.com>
2026-07-12 23:23:33 +0530

    random_concept

---
+++ bill_pay.py
@@ -0,0 +1,10 @@
+import random
+
+list_bill = ['Amit','Mansur','Harsh','Pratik','Vivaswan']
+#option1
+print(random.choice(list_bill))
+
+#option2
+random_int=random.randint(1,4)
+
+print(list_bill[random_int])
